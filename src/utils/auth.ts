c2566deeebe364a60d0195b7ac76f70ca7539736
Paul Mojica <paulmojicatech@gmail.com>
2024-02-20 08:11:37 -0500

    post form works

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,5 +1,4 @@
-import type { User } from '../models/auth/auth.interface';
-
+import type { RegisterHttpPostRequest, User } from '../models/auth/auth.interface';
 export function getCurrentUser(): User | undefined {
   if (localStorage?.getItem('currentUser')) {
     return JSON.parse(localStorage.getItem('currentUser') as string);
@@ -8,4 +7,20 @@ export function getCurrentUser(): User | undefined {
   }
 }
 
+export async function registerUser(user: RegisterHttpPostRequest): Promise<boolean> {
+  const url = 'https://localhost:5001/auth/register';  
+  try {
+    const options = {
+      method: 'POST',
+      headers: {'Content-Type': 'application/json', 'Accept': 'application/json', 'Access-Control-Allow-Origin': '*', 'Access-Control-Allow-Credentials': 'true', 'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS'},      
+      body: JSON.stringify(user),
+    }
+    const response = await fetch(url, options);
+    return Promise.resolve(!!response);
+  } catch (error) {
+    console.error('Error registering user', error);
+    return Promise.resolve(false);
+  }
+  
+}
 
