67aaf5a0bd0067182009708134f8cbca30c95be0
Paul Mojica <paulmojicatech@gmail.com>
2024-02-24 06:44:14 -0500

    add awsAmplify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,8 @@
 import { defineConfig } from 'astro/config';
-
+import awsAmplify from '@astrojs/adapter-aws-amplify';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'hybrid'
+  output: 'hybrid',
+  adapter: awsAmplify()
 });
