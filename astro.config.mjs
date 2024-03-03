a81b2413e5b618037b724260bbb42a03125a6a5d
Paul Mojica <paulmojicatech@gmail.com>
2024-03-03 15:48:22 -0500

    use aws amplify package again

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,8 @@
 import { defineConfig } from 'astro/config';
-
-import node from "@astrojs/node";
+import awsAmplify from 'astro-aws-amplify';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'hybrid',
-  adapter: node({
-    mode: "standalone"
-  })
+  adapter: awsAmplify()
 });
