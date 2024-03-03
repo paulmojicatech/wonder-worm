91bc1d858157f3c82f93a39859b72113bdec85ac
Paul Mojica <paulmojicatech@gmail.com>
2024-03-03 15:14:37 -0500

    move back to all client side

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,4 @@
 import { defineConfig } from 'astro/config';
-import awsAmplify from 'astro-aws-amplify';
 
 // https://astro.build/config
-export default defineConfig({
-  output: 'hybrid',
-  adapter: awsAmplify(),
-  outDir: './.amplify-hosting'
-});
+export default defineConfig({});
