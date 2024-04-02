af923861ea060d20f5ec9d20b0431cdcc439ce50
Paul Mojica <paulmojicatech@gmail.com>
2024-04-02 19:06:26 -0400

    add layout

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -1,4 +1,4 @@
-import type { RegisterHttpPostRequest, User } from '../models/auth/auth.interface';
+import type { LoginHttpPostRequest, RegisterHttpPostRequest, User } from '../models/auth/auth.interface';
 
 
 export function getCurrentUser(): User | undefined {
@@ -25,6 +25,40 @@ export async function registerUser(user: RegisterHttpPostRequest): Promise<{isSu
     console.error('Error registering user', error);   
     return Promise.resolve({isSuccess: false, message: 'Error registering user'});
   }
-  
+}
+
+export async function login(request: LoginHttpPostRequest): Promise<User & {isSuccess: boolean} | { isSuccess: boolean }> {
+  const url = 'https://tv5zym6bmx3cdif7z33tnztzdm0kjmyd.lambda-url.us-east-1.on.aws/auth/login';
+  try {
+    const options = {
+      method: 'POST',
+      headers: {'Content-Type': 'application/json', 'Accept': 'application/json', 'Access-Control-Allow-Origin': '*', 'Access-Control-Allow-Credentials': 'true', 'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS'},
+      body: JSON.stringify(request),
+    }
+    const response = await fetch(url, options);
+    const isSuccess = response.ok;
+    if (!isSuccess) {
+      console.error('Error logging in', await response.json());
+      return Promise.resolve({isSuccess: false});
+    }
+    const data = await response.json();
+    const user: User = {
+      userName: data.name,
+      lastLoggedIn: new Date().toISOString(),
+      email: request.email,
+      token: data.token,
+      children: data.children ?? [],
+    };
+    return Promise.resolve({...user, isSuccess: isSuccess});
+  } catch (error) {
+    console.error('Error logging in', error);
+    return Promise.resolve({isSuccess: false});
+  }
+}
+
+export function setCurrentUser(user: User): void {
+  if (document.cookie?.includes('user=')) {
+    localStorage.setItem('currentUser', JSON.stringify(user));
+  }
 }
 
