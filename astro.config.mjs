8e416fe3fd1d90d3e6034b14a86962dfcd2dc5ac
Paul Mojica <paulmojicatech@gmail.com>
2024-06-24 22:50:46 -0400

    add angular components to comprehension

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 import { defineConfig } from 'astro/config';
 import node from "@astrojs/node";
 
+import analogjsangular from "@analogjs/astro-angular";
+
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
   adapter: node({
     mode: "standalone"
-  })
+  }),
+  integrations: [analogjsangular()]
 });
